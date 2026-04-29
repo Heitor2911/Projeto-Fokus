73293c4efedb1f2475f0f421377b5c0daf4f911a
Heitor Lacourt <heitorlmsantos@gmail.com>
2026-04-29 14:22:21 -0300

    Exibi o formulário de tarefa

---
+++ script-crud.js
@@ -0,0 +1,8 @@
+// encontrar o botão adicionar tarefa
+
+const btnAddTask = document.querySelector('.app__button--add-task')
+const formAddTask = document.querySelector('.app__form-add-task')
+
+btnAddTask.addEventListener('click', () => {
+    formAddTask.classList.toggle('hidden')
+})
